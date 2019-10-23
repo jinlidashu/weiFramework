b571a17f585e454e013011741c9177635b02c823
李维亮 <4069320591@qq.com>
2019-10-23 18:17:31 +0800

    add stylus

---
+++ src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="liang-layout">
+        <div class="liang-top-aside">
+
+        </div>
+        <div class="liang-content-main">
+            <div class="liang-left-aside">
+                <liang_menu_aside />
+            </div>
+            <div class="liang-right-aside">
+                <router-view/>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import liang_menu_aside from '@/components/liang_menu_aside'
+    export default {
+        name: "index",
+        components:{
+            liang_menu_aside
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
