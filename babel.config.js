8a4a4f89200fe5344c700e30633c020acc4f3a71
李维亮 <4069320591@qq.com>
2020-01-16 17:41:28 +0800

    vue

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,9 @@
-const plugins = ["@vue/babel-plugin-transform-vue-jsx"];
+const plugins = [];
 // 生产环境移除console
 if(process.env.NODE_ENV === 'production') {
   plugins.push("transform-remove-console")
 }
 module.exports = {
   plugins: plugins,
-  presets: [
-    [
-      '@vue/app', {
-      modules: false,
-      targets: {
-        browsers: ["> 1%", "last 2 versions", "not ie <= 8", "Android >= 4", "iOS >= 8"]
-      },
-      useBuiltIns: 'entry',
-    }
-    ]
-  ]
-}
-// module.exports = {
-//   presets: [
-//     '@vue/cli-plugin-babel/preset'
-//   ]
-// }
+  presets: ["@vue/cli-plugin-babel/preset"]
+};
